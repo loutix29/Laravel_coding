5fbf505da3b678840aff9e988aed9b8397e6c63b
Bleine <bleine.dhellot@epitech.com>
2020-11-08 22:05:47 +0100

    header update

--- Free_ads/resources/views/about/contact.blade.php
+++ Free_ads/resources/views/about/contact.blade.php
@@ -29,7 +29,7 @@
           <div class="card mb-3" style="max-width: 540px;">
             <div class="row no-gutters">
               <div class="col-md-4">
-                <img src="{{ asset('images/avatar2.webp') }}" class="card-img" alt="...">
+                <img src="{{ asset('images/avatar-3.png') }}" class="card-img" alt="...">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
